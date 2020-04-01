8c5504eab286f3d3ec84409579475f42f89551dc
theodor1611 <48147912+theodor1611@users.noreply.github.com>
2020-04-01 17:50:51 +0300

    Laborator7/Inceputul proiectului 2
    Aminceput sa lucrez la P2 in laboratorul 7

---
+++ MyPhotosP2/MyPhotosP2/DTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPhotosP2
+{
+    public class DTO
+    {
+
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public string PozaSauVideo { get; set; }
+        public string Sters { get; set; }
+    }
+}
