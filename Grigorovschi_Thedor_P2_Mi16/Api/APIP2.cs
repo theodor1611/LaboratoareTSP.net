8893fca7cba9f8e64c8a628eb65dcee987021785
theodor1611 <48147912+theodor1611@users.noreply.github.com>
2020-04-21 14:35:56 +0300

    Proiectul 2
    Proiect WCF

---
+++ Grigorovschi_Thedor_P2_Mi16/Api/APIP2.cs
@@ -0,0 +1,159 @@
+﻿using MyPhotosP2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryMyPhotosP2
+{
+    public class APIP2
+    {
+        public ModelDBContainer context;
+        public APIP2()
+        {
+            this.context = new ModelDBContainer();
+        }
+        //metoda care preia path ul dat de utilizator si il pune in baza de date
+        public void PreluarePoza(string path)
+        {
+            int maxId = 0;
+            string ok = "3";
+            List<string> photoExtension = new List<string>() { ".tif", ".jpg", ".gif", ".png" };
+            List<string> videoExtension = new List<string>() { ".avchid", ".avi", ".flv", ".mkv", ".mov", ".mp4" };
+            string extension = Path.GetExtension(path).ToLower();
+            if (context.Media.Count() == 0)
+                maxId = 0;
+            else
+                maxId = context.Media.Max(a => a.Id);
+            if (photoExtension.Contains(extension))
+                ok = "0";
+            else if (videoExtension.Contains(extension))
+                ok = "1";
+            if (!ok.Equals("3"))
+            {
+                Media media = new Media()
+                {
+                    Id = maxId + 1,
+                    Path = path,
+                    PozaSauVideo = ok,
+                    Sters = "0"
+                };
+                context.Media.Add(media);
+                context.SaveChanges();
+            }
+        }
+        public void AdaugaProprietate(string path, string nume, string valoare)
+        {
+            Proprietati p = new Proprietati();
+            int exista = 0;
+            foreach (Proprietati prop in context.Proprietatis)
+            {
+                if (prop.NumeCamp.Equals(nume) && prop.ValoareCamp.Equals(valoare))
+                {
+                    exista = 1;
+                    p = prop;
+                }
+            }
+            var result = context.Media.FirstOrDefault(a => a.Path == path);
+            if (result != null)
+            {
+                if (exista == 0)
+                {
+                    Proprietati proprietate = new Proprietati()
+                    {
+                        Id = MaxIdProp() + 1,
+                        NumeCamp = nume,
+                        ValoareCamp = valoare
+                    };
+                    result.Proprietatis.Add(proprietate);
+                    context.SaveChanges();
+                }
+                else if (exista == 1)
+                {
+                    result.Proprietatis.Add(p);
+                    context.SaveChanges();
+                }
+            }
+        }
+        public void StergeProprietate(string path, string nume, string valoare)
+        {
+            var result = context.Media.FirstOrDefault(a => a.Path == path);
+            if (result != null)
+            {
+                foreach (Proprietati prop in result.Proprietatis)
+                {
+                    if (prop.NumeCamp.Equals(nume) && prop.ValoareCamp.Equals(valoare))
+                    {
+                        result.Proprietatis.Remove(prop);
+                        break;
+                    }
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public void SchimbaProprietate(string nume, string valoare)
+        {
+            var result = context.Proprietatis.FirstOrDefault(a => a.NumeCamp == nume);
+            if (result != null)
+            {
+                result.ValoareCamp = valoare;
+                context.SaveChanges();
+            }
+        }
+
+        public void StergePoza(string path)
+        {
+            var result = context.Media.FirstOrDefault(a => a.Path == path);
+            if (result != null)
+            {
+                result.Sters = "1";
+                context.SaveChanges();
+            }
+        }
+
+        public List<DTO> Cautare(string nume, string valoare)
+        {
+            List<DTO> gasite = new List<DTO>();
+
+            var result = context.Proprietatis.FirstOrDefault(a => a.NumeCamp == nume && a.ValoareCamp == valoare);
+            if (result != null)
+            {
+                context.Entry(result).Collection(r=>r.Media).Load();
+                foreach (Media media in result.Media)
+                {
+                    DTO dto = new DTO()
+                    {
+                        Id = media.Id,
+                        Path = media.Path,
+                        PozaSauVideo = media.PozaSauVideo,
+                        Sters = media.Sters
+                    };
+                    gasite.Add(dto);
+                }
+            }
+            return gasite;
+        }
+
+        public int MaxIdMedia()
+        {
+            int maxId = 0;
+            if (context.Media.Count() == 0)
+                maxId = 0;
+            else
+                maxId = context.Media.Max(a => a.Id) + 1;
+            return maxId;
+        }
+        public int MaxIdProp()
+        {
+            int maxId = 0;
+            if (context.Proprietatis.Count() == 0)
+                maxId = 0;
+            else
+                maxId = context.Proprietatis.Max(a => a.Id) + 1;
+            return maxId;
+        }
+    }
+}
---
+++ Grigorovschi_Thedor_P2_Mi16/Client/Form1.cs
@@ -0,0 +1,97 @@
+﻿using MyPhotosP2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        //InterfaceApiClient client = new InterfaceApiClient();
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.CellDoubleClick += dataGridView1_CellDoubleClick;
+        }
+
+        private void buttonMemorare_Click(object sender, EventArgs e)
+        {
+            InterfaceApiClient client = new InterfaceApiClient();
+            if (textBox1.Text != null)
+            {
+                client.PreluarePoza(textBox1.Text);
+                textBox1.Text = "";
+                client.Close();
+            }
+        }
+
+        private void buttonCautare_Click(object sender, EventArgs e)
+        {
+            InterfaceApiClient client = new InterfaceApiClient();
+            //List<DTO> rezultate = new List<DTO>();
+            if (textBox2.Text != null && textBox3.Text != null)
+            {
+                //rezultate = client.Cautare(textBox2.Text, textBox3.Text);
+                var bindingList = new BindingList<DTO>(client.Cautare(textBox2.Text, textBox3.Text));
+                var source = new BindingSource(bindingList, null);
+                dataGridView1.DataSource = source;
+                client.Close();
+            }
+        }
+
+        private void buttonAddProp_Click(object sender, EventArgs e)
+        {
+            InterfaceApiClient client = new InterfaceApiClient();
+            if (textBox1.Text != null && textBox2.Text != null && textBox3.Text != null)
+            {
+                client.AdaugaProprietate(textBox1.Text, textBox2.Text, textBox3.Text);
+                client.Close();
+            }
+        }
+
+        private void buttonDelProp_Click(object sender, EventArgs e)
+        {
+            InterfaceApiClient client = new InterfaceApiClient();
+            if (textBox1.Text != null && textBox2.Text != null && textBox3.Text != null)
+            {
+                client.StergeProprietate(textBox1.Text, textBox2.Text, textBox3.Text);
+                client.Close();
+            }
+        }
+
+        private void buttonDelPhoto_Click(object sender, EventArgs e)
+        {
+            InterfaceApiClient client = new InterfaceApiClient();
+            if (textBox1.Text != null)
+            {
+                client.StergePoza(textBox1.Text);
+                client.Close();
+            }
+        }
+
+        private void buttonChangeProp_Click(object sender, EventArgs e)
+        {
+            InterfaceApiClient client = new InterfaceApiClient();
+            if (textBox2.Text != null && textBox3.Text != null)
+            {
+                client.SchimbaProprietate(textBox2.Text, textBox3.Text);
+                client.Close();
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string fullPath = this.dataGridView1.CurrentRow.Cells[2].Value.ToString();
+
+            Process.Start(fullPath);
+        }
+
+    }
+}
